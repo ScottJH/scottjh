d30b94812fe842db276eabf579d7704ade97da51
Scott Haselwood <scotthaselwood@Scotts-MBP.domain>
2019-04-20 11:18:11 -0700

    added headshot

--- src/templates/Home.js
+++ src/templates/Home.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
-import Banner from '../blocks/Banner.js';
+import {Banner, ImageContent, Columns} from '../blocks';
 
 class Home extends Component {
     render() {
         return (
             <div className="wrapper">
-                <Banner title="Home"/>
+                <Banner title="Scott J Haselwood"/>
                 <div className="main-container" role="main">
-                    <div className="row">
-                        <div className="col full">
-                            <p>Hello! My name is Scott J Haselwood.</p>
-                            <p>I am a web developer living in Portland, OR. Contact me if you like what you see.</p>
-                        </div>
-                    </div>
+                <ImageContent
+                        imageSide="left"
+                        image={require('../assets/images/headshot@2x.jpg')}
+                        heading="Hello!"
+                        content="My name is Scott J Haselwood. I am a web developer living in Portland, OR. Contact me if you like what you see."
+                    />
                 </div>
             </div>
         )
