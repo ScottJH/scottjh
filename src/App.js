b40510b7130334ec4a388cffa50aa92fe51edaf5
Scott Haselwood <scotthaselwood@Scotts-MBP.domain>
2020-04-16 10:38:39 -0700

    feat: added privacy policy

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Home from './templates/Home'
 import Styleguide from './templates/Styleguide'
 import Blocks from './templates/Blocks'
+import Privacy from './templates/Privacy'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -25,6 +26,7 @@ class App extends Component {
                           component={Home} />
                       <Route path="/styleguide" component={Styleguide} />
                       <Route path="/blocks" component={Blocks} />
+                      <Route path="/privacy" component={Privacy} />
                       <Route component={NotFound} />
                   </Switch>
                 <Footer />
